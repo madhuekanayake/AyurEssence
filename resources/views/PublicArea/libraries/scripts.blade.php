3556dff8b094ddc01ce09163fe8a7c478388c583
Nisansala Ekanayake <153098559+madhuekanayake@users.noreply.github.com>
2025-01-20 20:29:35 +0530

    Update

--- resources/views/PublicArea/libraries/scripts.blade.php
+++ resources/views/PublicArea/libraries/scripts.blade.php
@@ -17,12 +17,32 @@
 <script src="{{ asset('PublicArea/js/aos.js') }}"></script>
 
 
-<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 
-
-
-
-
+<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="{{ asset('AdminArea/vendors/js/vendor.bundle.base.js') }}"></script>
+<script src="{{ asset('AdminArea/vendors/js/vendor.bundle.addons.js') }}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="{{ asset('AdminArea/js/off-canvas.js') }}"></script>
+<script src="{{ asset('AdminArea/js/hoverable-collapse.js') }}"></script>
+<script src="{{ asset('AdminArea/js/misc.js') }}"></script>
+<script src="{{ asset('AdminArea/js/settings.js') }}"></script>
+<script src="{{ asset('AdimnArea/js/todolist.js') }}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{ asset('AdminArea/js/dashboard.js') }}"></script>
+<!-- End custom js for this page-->
+<script src="{{ asset('AdminArea/js/data-table.js') }}"></script>
+<script src="{{ asset('AdminArea/js/modal-demo.js') }}"></script>
+<script src="{{ asset('AdminArea/js/file-upload.js') }}"></script>
+  <script src="{{ asset('AdminArea/js/typeahead.js') }}"></script>
+  <script src="{{ asset('AdminArea/js/select2.js') }}"></script>
+  <script src="{{ asset('AdminArea/js/wizard.js') }}"></script>
+  <script src="{{ asset('AdminArea/js/editorDemo.js') }}"></script>
 
 
 
@@ -70,6 +90,4 @@ AOS.init({
 
 
 @stack('js')
-
-
 
