740437276b2256a8eef5d3eb7ddab96625285111
Nisansala Ekanayake <153098559+madhuekanayake@users.noreply.github.com>
2025-01-20 20:55:49 +0530

    Update libraries files

--- resources/views/PublicArea/libraries/styles.blade.php
+++ resources/views/PublicArea/libraries/styles.blade.php
@@ -1,4 +1,4 @@
-{{-- <link rel="shortcut icon" href="{{ asset('PublicArea/images/favicon.png') }}">
+<link rel="shortcut icon" href="{{ asset('PublicArea/images/favicon.png') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/bootstrap.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/animate.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/all.min.css') }}">
@@ -13,35 +13,13 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/shortcodes.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/main.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/megamenu.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/responsive.css') }}"> --}}
-
-
-<link rel="shortcut icon" href="{{ 'PublicArea/images/favicon.png' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/bootstrap.min.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/animate.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/all.min.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/fontello.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/flaticon.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/themify-icons.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/font-awesome.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/aos.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/slick.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/prettyPhoto.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/samarkannormal.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/shortcodes.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/main.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/megamenu.css' }}">
-<link rel="stylesheet" type="text/css" href="{{ 'PublicArea/css/responsive.css' }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('PublicArea/css/responsive.css') }}">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
 
 
 {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.5/swiper-bundle.min.css"> --}}
-
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.5/swiper-bundle.min.css">
 
 
 @stack('css')
